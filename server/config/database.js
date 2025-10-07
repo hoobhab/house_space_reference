cb810a38bc52c0accc08b9dca4dfd6f70bb7401e
hoobhab <hoobhab3@gmail.com>
2025-10-07 13:46:37 -0700

    Added docstring to pool object function

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,11 @@
 import pg from 'pg'
 
 const config = {
+    /**
+     * Creates a PostgreSQL connection pool object so that the server can 
+     * create new client connections as needed
+     */
+    
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     host: process.env.PGHOST,
